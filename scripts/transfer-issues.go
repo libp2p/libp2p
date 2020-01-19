1edb0d6c63cb4cb6abba016365b652ebe8d52002
Aarsh Shah <aarshkshah1992@gmail.com>
2020-01-19 17:01:23 +0530

    moved out responses

--- scripts/transfer-issues.go
+++ scripts/transfer-issues.go
@@ -10,6 +10,18 @@ import (
 	"strings"
 )
 
+type Edge struct {
+	Cursor string
+	Node   struct {
+		Name string
+		Id   string
+	}
+}
+
+type Issue struct {
+	Id string
+}
+
 type Config struct {
 	Owner              string
 	RedirectIssueTitle string
